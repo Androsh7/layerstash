9e30e436ff3739bc76be5d2983da17ad82591090
androsh7@gmail.com <androsh7@gmail.com>
2026-02-18 11:14:07 -0700

    Added file listing functionality

--- layerstash/main.py
+++ layerstash/main.py
@@ -12,6 +12,7 @@ from loguru import logger
 from layerstash.constants import LOG_LEVELS, VERSION, config, endpoint_dict
 from layerstash.download import download_file_from_images
 from layerstash.upload import push_file_as_image_chunks
+from layerstash.list_files import list_files
 
 
 def main():
@@ -31,19 +32,14 @@ def main():
         "-r", "--repository", required=True, type=str, help="Name of the remote repository, i.e., androsh7/archive"
     )
     common_parser.add_argument(
-        "-t",
-        "--base-tag",
-        required=True,
-        type=str,
-        help='The base tag, i.e., "python-ftp" , each chunk will have "-<INDEX>" appended to the end',
-    )
-    common_parser.add_argument(
         "--registry",
         type=str,
         default="docker",
         choices=list(endpoint_dict.keys()),
         help=f'The registry to use {tuple(endpoint_dict.keys())}, default: "docker"',
     )
+    common_parser.add_argument("-u", "--username", required=False, type=str, help="Docker username")
+    common_parser.add_argument("-p", "--token", required=False, type=str, help="Docker PAT token")
 
     # Upload parser
     upload_parser = subparsers.add_parser(
@@ -53,8 +49,13 @@ def main():
     upload_parser.add_argument(
         "--overwrite", action="store_true", help="Overwrite existing images if they have a different hash"
     )
-    upload_parser.add_argument("-u", "--username", required=True, type=str, help="Docker username")
-    upload_parser.add_argument("-p", "--token", required=True, type=str, help="Docker PAT token")
+    upload_parser.add_argument(
+        "-t",
+        "--base-tag",
+        required=True,
+        type=str,
+        help='The base tag, i.e., "python-ftp" , each chunk will have "-<INDEX>" appended to the end',
+    )
 
     # Download parser
     download_parser = subparsers.add_parser(
@@ -63,8 +64,19 @@ def main():
     download_parser.add_argument(
         "-o", "--outfile", required=True, type=Path, help="The file to write the downloaded chunks to"
     )
-    download_parser.add_argument("-u", "--username", required=False, type=str, help="Docker username")
-    download_parser.add_argument("-p", "--token", required=False, type=str, help="Docker PAT token")
+    download_parser.add_argument(
+        "-t",
+        "--base-tag",
+        required=True,
+        type=str,
+        help='The base tag, i.e., "python-ftp" , each chunk will have "-<INDEX>" appended to the end',
+    )
+
+    # List files parser
+    list_files_parser = subparsers.add_parser(
+        "list", parents=[common_parser], help="Lists files",
+    )
+    list_files_parser.add_argument("--exact-size", action="store_true", help="Returns the exact size of the files")
 
     args = parser.parse_args()
 
@@ -74,7 +86,7 @@ def main():
 
     # Set the config
     config.repository = args.repository
-    config.base_tag = args.base_tag
+    config.base_tag = getattr(args, "base_tag", None)
 
     # Set login details
     if args.token or args.username:
@@ -86,12 +98,15 @@ def main():
     # Set endpoint
     config.endpoints = endpoint_dict[args.registry]
 
-    if args.command == "upload":
+    if args.command == "list":
+        list_files(args.exact_size)
+    elif args.command == "upload":
         config.overwrite_image = args.overwrite
         push_file_as_image_chunks(file_path=args.infile)
-    if args.command == "download":
+    elif args.command == "download":
         download_file_from_images(out_file_path=args.outfile)
-
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
